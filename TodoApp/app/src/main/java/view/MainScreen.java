2d691bb5df468c58ed4a69b5c3d7337b2bbdc186
Ana Karine <anakarine.ti@gmail.com>
2023-11-29 21:10:02 -0300

    implementando CellRenderer Editar e Deletar

--- TodoApp/app/src/main/java/view/MainScreen.java
+++ TodoApp/app/src/main/java/view/MainScreen.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.swing.DefaultListModel;
 import model.Project;
 import model.Task;
+import util.ButtonColumnCellRenderer;
 import util.DeadlineColumnCellRenderer;
 import util.TaskTableModel;
 
@@ -492,6 +493,12 @@ public class MainScreen extends javax.swing.JFrame {
         
         jTableTasks.getColumnModel().getColumn(2)
                 .setCellRenderer(new DeadlineColumnCellRenderer());
+        
+        jTableTasks.getColumnModel().getColumn(4)
+                .setCellRenderer(new ButtonColumnCellRenderer("edit"));
+        
+        jTableTasks.getColumnModel().getColumn(5)
+                .setCellRenderer(new ButtonColumnCellRenderer("delete"));
         
         // criando um sort automático para as colunas da tabela
 //        if (jTableTasks.getRowCount() > 0) {
