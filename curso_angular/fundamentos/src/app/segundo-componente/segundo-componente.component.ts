2dcc54a562651adf265921e8076cd8ba139fec93
Ana Karine <anakarine.ti@gmail.com>
2023-12-12 16:35:58 -0300

    property binding e event binding

--- curso_angular/fundamentos/src/app/segundo-componente/segundo-componente.component.ts
+++ curso_angular/fundamentos/src/app/segundo-componente/segundo-componente.component.ts
@@ -5,9 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './segundo-componente.component.html',
   styleUrls: ['./segundo-componente.component.css']
 })
+
 export class SegundoComponenteComponent {
-
   nome = "Ana";
-  dataNascimento = "1981-01-15"
+  dataNascimento = "1981-01-15";
+  urlImagem = "/assets/avatar.png"
 
+  mostrarDataNascimento() {
+    alert(`A data de nascimento do João é: ${this.dataNascimento}`)
+  }
 }
