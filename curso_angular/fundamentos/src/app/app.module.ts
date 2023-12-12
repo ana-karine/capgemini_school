ece5a0e80b65642d1845d64c5114a0f18886d76e
Ana Karine <anakarine.ti@gmail.com>
2023-12-12 11:05:10 -0300

    segundo componente a partir do comando ng generate

--- curso_angular/fundamentos/src/app/app.module.ts
+++ curso_angular/fundamentos/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimeiroComponenteComponent
+    PrimeiroComponenteComponent,
+    SegundoComponenteComponent
   ],
   imports: [
     BrowserModule
---
+++ curso_angular/fundamentos/src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  templateUrl: './segundo-componente.component.html',
+  styleUrls: ['./segundo-componente.component.css']
+})
+export class SegundoComponenteComponent {
+
+  nome = "Ana";
+  dataNascimento = "1981-01-15"
+
+}
