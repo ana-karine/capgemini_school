c518274c14a59422c9df5ee7dcfd82c9b9dcc291
Ana Karine <anakarine.ti@gmail.com>
2023-12-13 09:08:23 -0300

    componente personalizado

--- curso_angular/fundamentos/src/app/app.module.ts
+++ curso_angular/fundamentos/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.co
 import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
 import { FormsModule } from '@angular/forms';
 import { RenderizandoListasComponent } from './renderizando-listas/renderizando-listas.component';
+import { ComponentePersonalizadoComponent } from './componente-personalizado/componente-personalizado.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { RenderizandoListasComponent } from './renderizando-listas/renderizando-
     MultiplicaPorPipe,
     PipeCustomizadoComponent,
     TwoWayDataBindingComponent,
-    RenderizandoListasComponent
+    RenderizandoListasComponent,
+    ComponentePersonalizadoComponent
   ],
   imports: [
     BrowserModule,
